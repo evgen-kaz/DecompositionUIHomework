5452c8ac3a6cd56bce2c78a43473671c44fb6f5b
evgen-kaz <itroute@mail.ru>
2025-11-06 10:53:23 +0800

    added Attach

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -4,12 +4,12 @@ import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
-import static com.codeborne.selenide.Selenide.sessionId;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
@@ -40,18 +40,21 @@ public class Attach {
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo() {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-                + getVideoUrl()
+                + getVideoUrl(getSessionId()) //указываем путь к этому видео
                 + "' type='video/mp4'></video></body></html>";
     }
 
-    public static URL getVideoUrl() {
-        String webDriverHost = System.getProperty("webDriverHost");
-        String videoUrl = "https://" + webDriverHost + "/video/" + sessionId() + ".mp4";
+    public static URL getVideoUrl(String sessionId) {
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId + ".mp4";
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static String getSessionId() { //получаем sessionId
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
     }
 }
