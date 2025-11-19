db069512f8a85480ab93650212f88e56d52edc7d
evgen-kaz <141723080+evgen-kaz@users.noreply.github.com>
2025-11-19 14:38:38 +0800

    Delete src/test/java/api/EndPoint.java

--- src/test/java/api/EndPoint.java
+++
@@ -1,7 +0,0 @@
-package api;
-
-public class EndPoint {
-    public static final String BOOKS_WITH_USER_ID = "/BookStore/v1/Books?UserId=";
-    public static final String BOOKS_END_POINT = "/BookStore/v1/Books";
-    public static final String LOGIN_END_POINT = "/Account/v1/Login";
-}
